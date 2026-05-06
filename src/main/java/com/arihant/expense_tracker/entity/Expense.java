7cb3f575535b5bf3c682284751cde1ee440557f3
ArihantVyas <arihantvyas@gmail.com>
2026-05-06 16:47:52 +0530

    Updated the enity
    Added new fields and updated existing fields. Also added more validations

--- src/main/java/com/arihant/expense_tracker/entity/Expense.java
+++ src/main/java/com/arihant/expense_tracker/entity/Expense.java
@@ -2,8 +2,11 @@ package com.arihant.expense_tracker.entity;
 
 import com.arihant.expense_tracker.enums.TransactionType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -13,19 +16,37 @@ public class Expense {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+   @NotBlank(message = "Title name must be provided")
     private String title;
 
+    // Will only accept these listed values. Requires validation handling in controller
     @Pattern(regexp = "Food|Travel|Shopping|Entertainment|Work|Rent|Gym|Bills|Subscriptions")
     private String category;
 
+    // It will also only accept the values defined in enum. Also requires validation handling in controller
     @Enumerated(EnumType.STRING)
     private TransactionType type;
 
+    @Positive(message = "Amount must be greater than 0")
     private Double amount;
-    
-    private LocalDateTime time;
 
+    private LocalDate expenseDate;
+
+    private LocalDateTime entryDateTime;
+
+    /* A no-args constructor is required by the JPA and Hibernate because it doesn't know which fields are to be passed,
+     So it initially creates an empty object.
+     Along with that it requires getters and setters to populate the empty object
+     */
     public Expense() {
+    }
+
+    public Expense(String title, String category, TransactionType type, Double amount, LocalDate expenseDate) {
+        this.title = title;
+        this.category = category;
+        this.type = type;
+        this.amount = amount;
+        this.expenseDate = expenseDate;
     }
 
     public Long getId() {
@@ -68,11 +89,21 @@ public class Expense {
         this.amount = amount;
     }
 
-    public LocalDateTime getTime() {
-        return time;
+    public LocalDate getExpenseDate() {
+        return expenseDate;
     }
 
-    public void setTime(LocalDateTime time) {
-        this.time = time;
+    public void setExpenseDate(LocalDate expenseDate) {
+        this.expenseDate = expenseDate;
+    }
+
+    public LocalDateTime getEntryDateTime() {
+        return entryDateTime;
+    }
+
+   // Runs only for the first time when entry is done
+    @PrePersist
+    public void setEntryDateTime() {
+        this.entryDateTime = LocalDateTime.now();
     }
 }
