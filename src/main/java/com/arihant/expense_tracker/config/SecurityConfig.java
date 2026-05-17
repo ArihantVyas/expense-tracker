022243775f96b39a21e4f09bcf0b64d192c3735d
ArihantVyas <arihantvyas@gmail.com>
2026-05-17 21:17:41 +0530

    Created SecurityConfig class
    Added Argon2PasswordEncoder bean and SecurityFilterChain bean to SecurityConfig class

---
+++ src/main/java/com/arihant/expense_tracker/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.arihant.expense_tracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new Argon2PasswordEncoder(16,32,1,65536,3);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http.csrf(csrfConfObj -> csrfConfObj.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("api/test","auth/register").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
