4ace6f4696b508d4502aa610ad0ad95441262749
ArihantVyas <arihantvyas@gmail.com>
2026-05-17 23:58:06 +0530

    Implemented the authentication logic for user
    Updated SecurityConfig , added findByUsername method in user repo , created a UserService and another custom Service for implementing UserDetailsService

---
+++ src/main/java/com/arihant/expense_tracker/service/UserDetailsServiceImplmt.java
@@ -0,0 +1,31 @@
+package com.arihant.expense_tracker.service;
+
+import com.arihant.expense_tracker.entity.User;
+import com.arihant.expense_tracker.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImplmt implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserDetailsServiceImplmt(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(username)
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
---
+++ src/main/java/com/arihant/expense_tracker/service/UserService.java
@@ -0,0 +1,36 @@
+package com.arihant.expense_tracker.service;
+
+import com.arihant.expense_tracker.dto.UserRegisterDto;
+import com.arihant.expense_tracker.entity.User;
+import com.arihant.expense_tracker.repository.UserRepository;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public UserService(PasswordEncoder passwordEncoder,UserRepository userRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+    }
+
+    public String registerUser(UserRegisterDto registerDto){
+
+        User newUser = new User();
+
+        newUser.setUsername(registerDto.getUsername());
+        newUser.setEmail(registerDto.getEmail());
+        newUser.setPassword(registerDto.getPassword());
+
+        newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
+
+        userRepository.save(newUser);
+
+        return "User registered successfully";
+    }
+
+}
